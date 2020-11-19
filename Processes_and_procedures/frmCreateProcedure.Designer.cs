3158ca9b5e658ada202e9483be62ff7bb1a04b0d
Dylan MacDermot <dylan.macdermot@gmail.com>
2020-11-19 11:25:25 -0600

    Add files via upload

---
+++ Processes_and_procedures/frmCreateProcedure.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Processes_and_procedures
+{
+    partial class frmCreateProcedure
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCreateProcedure = new System.Windows.Forms.Label();
+            this.txtCreateProcedure = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.processesAndProcedures_DataSet1 = new Processes_and_procedures.ProcessesAndProcedures_DataSet();
+            this.proceduresTableAdapter1 = new Processes_and_procedures.ProcessesAndProcedures_DataSetTableAdapters.ProceduresTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.processesAndProcedures_DataSet1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblCreateProcedure
+            // 
+            this.lblCreateProcedure.AutoSize = true;
+            this.lblCreateProcedure.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCreateProcedure.Location = new System.Drawing.Point(12, 26);
+            this.lblCreateProcedure.Name = "lblCreateProcedure";
+            this.lblCreateProcedure.Size = new System.Drawing.Size(146, 20);
+            this.lblCreateProcedure.TabIndex = 0;
+            this.lblCreateProcedure.Text = "Create Procedure:";
+            // 
+            // txtCreateProcedure
+            // 
+            this.txtCreateProcedure.Location = new System.Drawing.Point(218, 26);
+            this.txtCreateProcedure.Name = "txtCreateProcedure";
+            this.txtCreateProcedure.Size = new System.Drawing.Size(412, 22);
+            this.txtCreateProcedure.TabIndex = 1;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubmit.Location = new System.Drawing.Point(304, 76);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(124, 36);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // processesAndProcedures_DataSet1
+            // 
+            this.processesAndProcedures_DataSet1.DataSetName = "ProcessesAndProcedures_DataSet";
+            this.processesAndProcedures_DataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // proceduresTableAdapter1
+            // 
+            this.proceduresTableAdapter1.ClearBeforeFill = true;
+            // 
+            // frmCreateProcedure
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(705, 152);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtCreateProcedure);
+            this.Controls.Add(this.lblCreateProcedure);
+            this.Name = "frmCreateProcedure";
+            this.Text = "Create Procedure";
+            ((System.ComponentModel.ISupportInitialize)(this.processesAndProcedures_DataSet1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCreateProcedure;
+        private System.Windows.Forms.TextBox txtCreateProcedure;
+        private System.Windows.Forms.Button btnSubmit;
+        private ProcessesAndProcedures_DataSet processesAndProcedures_DataSet1;
+        private ProcessesAndProcedures_DataSetTableAdapters.ProceduresTableAdapter proceduresTableAdapter1;
+    }
+}
---
+++ Processes_and_procedures/frmProcessesAndProcedures.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Processes_and_procedures
+{
+    public partial class frmProcessesAndProcedures : Form
+    {
+        public frmProcessesAndProcedures()
+        {
+            InitializeComponent();
+        }
+
+        private void frmProcessesAndProcedures_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'processesAndProcedures_DataSet.Procedures' table. You can move, or remove it, as needed.
+            this.proceduresTableAdapter.Fill(this.processesAndProcedures_DataSet.Procedures);
+
+        }
+
+        private void btnCreateProcedure_Click(object sender, EventArgs e)
+        {
+            frmCreateProcedure form1 = new frmCreateProcedure();
+
+            form1.Show();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            this.proceduresTableAdapter.Fill(this.processesAndProcedures_DataSet.Procedures);
+        }
+
+        private void btnDeleteProcedure_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Call the 'Delete1' ProcedureTableAdapter query.
+                this.proceduresTableAdapter.DeleteProcedure(Int32.Parse(dataGridView1.CurrentRow.Cells[0].Value.ToString()));
+
+                // 'Refresh' the procedures table
+                this.proceduresTableAdapter.Fill(this.processesAndProcedures_DataSet.Procedures);
+            }
+            catch (NullReferenceException)
+            {
+                MessageBox.Show("No more columns to delete.");
+            }
+        }
+
+        private void btnViewProcedure_Click(object sender, EventArgs e)
+        {
+
+            System.Data.DataRowView SelectedRowView;
+            ProcessesAndProcedures_DataSet.ProceduresRow SelectedRow;
+
+            SelectedRowView = (System.Data.DataRowView)proceduresBindingSource.Current;
+            SelectedRow = (ProcessesAndProcedures_DataSet.ProceduresRow)SelectedRowView.Row;
+
+            frmViewSteps stepsForm = new frmViewSteps(dataGridView1.CurrentRow.Cells[1].Value.ToString(), Int32.Parse(dataGridView1.CurrentRow.Cells[0].Value.ToString()));
+            stepsForm.LoadProcedures(SelectedRow.Procedure_ID);
+            stepsForm.Show();
+            
+        }
+    }
+}
